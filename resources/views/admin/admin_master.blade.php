5198abdcaf84730f9be829ba24214ce2fea3e423
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2022-01-07 14:26:52 +0200

    init commit

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -38,6 +38,7 @@
         </div>
         <!-- ./wrapper -->
 
+
         <!-- Vendor JS -->
         <script src="{{asset('backend/js/vendors.min.js')}}"></script>
         <script src="{{asset('../assets/icons/feather-icons/feather.min.js')}}"></script>
