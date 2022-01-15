14ea5b99eef070f4fc8b21fe775c817e743a2248
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2022-01-15 04:14:46 +0200

    Show hot deal

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\MultiImg;
 use App\Models\Product;
@@ -22,12 +23,19 @@ class IndexController extends Controller
         $sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
         $products = Product::where('status',1)->orderBy('id','DESC')->limit(6)->get();
         $featured = Product::where('featured',1)->orderBy('id','DESC')->limit(6)->get();
-        $hot_deals = Product::where('hot_deals',1)->orderBy('id','DESC')->limit(3)->get();
+        $hot_deals = Product::where('hot_deals',1)->where('discount_price','!=',NULL)->orderBy('id','DESC')->limit(3)->get();
         $special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(6)->get();
         $special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(3)->get();
+        $skip_category_0 = Category::skip(0)->first();
+        $skip_product_0 = Product::where('status',1)->where('category_id',$skip_category_0->id)->orderBy('id','DESC')->get();
+        $skip_category_1 = Category::skip(1)->first();
+        $skip_product_1 = Product::where('status',1)->where('category_id',$skip_category_1->id)->orderBy('id','DESC')->get();
+        $skip_brand_1 = Brand::skip(1)->first();
+        $skip_brand_product_1 = Product::where('status',1)->where('brand_id',$skip_brand_1->id)->orderBy('id','DESC')->get();
         return view('frontend.index',
             compact('categories', 'sliders', 'products', 'featured',
-                'hot_deals', 'special_offer', 'special_deals'
+                'hot_deals', 'special_offer', 'special_deals','skip_category_0','skip_product_0',
+                'skip_category_1','skip_product_1','skip_brand_1','skip_brand_product_1'
             ));
     }
 
