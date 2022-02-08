ec9993a7b4e58997c06e785970236e713b20500d
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2022-02-09 00:56:25 +0200

    update installment

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -40,7 +40,7 @@ class CheckoutController extends Controller
         if ($request->payment_method == 'stripe') {
             return view('frontend.payment.stripe',compact('data','cartTotal'));
         }elseif ($request->payment_method == 'card') {
-            return 'card';
+            return view('frontend.payment.installment',compact('data','cartTotal'));
         }else{
             return view('frontend.payment.cash',compact('data','cartTotal'));
         }
